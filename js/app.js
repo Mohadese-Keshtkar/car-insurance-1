78e46e3fd4b0c52cf203bee8bb2030e127964cdb
Mohadese-Keshtkar <mohadesekeshtkar4@gmail.com>
2023-10-06 15:43:48 +0330

    update app js(add nimber yearsmade)

--- js/app.js
+++ js/app.js
@@ -1,56 +1,56 @@
-// Variables
 
+function yearsFunction() {
 
+    //Convert string to number
 
-// Events
-
-
-
-// Functions
-// User Interface Functions
-// Show Years
-function displayYears() {
-    // Convert to number
     let
-        persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
-        arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+        iranainNumber = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+
+        arabicNumber = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+
         fixNumbers = function (str) {
+
             if (typeof str === 'string') {
-                for (var i = 0; i < 10; i++) {
-                    str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+
+                for (let i = 0; i < 10; i++) {
+
+                    str = str.replace(iranainNumber[i], i).replace(arabicNumber[i], i);
+
                 }
             }
-            return parseInt(str);
-        };
 
-    // get now years
+            return parseInt(str);
+        }
+
+
+
+    //get new year
     let curentYear = new Date().toLocaleDateString('fa-IR')
 
-    // Slice date
+    //Slice year Data
     curentYear = curentYear.slice(0, 4)
 
-    // get max year
+    //get max year
     let maxYear = fixNumbers(curentYear)
-    console.log(maxYear);
 
-    // get min year
+    console.log(maxYear)
+
+
+    //get min year
     let minYear = maxYear - 20
-    console.log(minYear);
-
-    // access to the select tag
+    console.log(minYear)
     const selectYear = document.querySelector('#year')
 
-    // create for loop for making option tag
+    //create option for loop
     for (let i = maxYear; i >= minYear; i--) {
-        // create option tag
-        const optionTag = document.createElement('option')
-        optionTag.value = i;
-        optionTag.innerText = `سال ${i}`;
+        // creat tag 
+        const tagOption = document.createElement('option')
+        tagOption.value = i;
+        tagOption.innerText = `سال ${i}`
 
-        // append option to the selectYear
-        selectYear.appendChild(optionTag)
+        //append option tag
+        selectYear.appendChild(tagOption)
     }
 }
 
-
-displayYears()
+yearsFunction()
